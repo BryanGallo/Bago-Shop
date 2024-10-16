8e190a32cf2896623f69b62b2e9e59dbfcdbd5e3
BryanGallo <bryangallo95@hotmail.com>
2024-10-16 00:00:23 -0500

    Configuranto conexion a las BDD con TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true, //
+      synchronize: true,// en produccion no colocarlo en true ya que sincroniza apenas se haga algo
+    }),
+  ],
 })
 export class AppModule {}
