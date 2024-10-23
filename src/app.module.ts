984cea81e34ee49453bf79d33317ce97caeafdbd
BryanGallo <bryangallo95@hotmail.com>
2024-10-23 00:10:03 -0500

    Creando RestAPI Products con nest g res

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true, //
       synchronize: true,// en produccion no colocarlo en true ya que sincroniza apenas se haga algo
     }),
+    ProductsModule,
   ],
 })
 export class AppModule {}
