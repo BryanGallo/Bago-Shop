cecab3fc2b56c727241b39e807d5ef58e0f5c427
BryanGallo <bryangallo95@hotmail.com>
2024-10-23 01:02:56 -0500

    Implementando CreateProductDto con class-validator
    * Se utiliza los decoradores de class-validator

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,43 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsNumber() //a diferencia de int puede recibir decimales
+  @IsOptional()
+  @IsPositive()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsOptional()
+  @IsPositive()
+  stock?: number;
+
+  @IsString({ each: true }) // indica que cada uno de los elementos del arreglo deben ser string
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'], {
+    message: 'No es un genero valido',
+  }) //indica que debe estar dentro de cualquier valor del arreglo o no puede ingresar
+  gender?: string;
+}
