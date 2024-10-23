780852c4a6741f896171e089a0a8c61923414c0b
BryanGallo <bryangallo95@hotmail.com>
2024-10-23 00:41:15 -0500

    Agregando atributos a la entidad Product
    * Indicando a prettier el espaciado del proyecto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,12 +1,44 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column('text',{
-        unique:true,
-    })
-    title:string;
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  //otra forma de especificar el tipo con type y usando un solo objeto
+  @Column({
+    type: 'float',
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  slug: string;
+
+  @Column({ type: 'int' })
+  stock: number;
+
+  @Column({
+    type: 'text',
+    array: true, //Con esta opcion se vuelve un arreglo segun el tipo de dato
+  })
+  sizes: string[];
+
+  @Column({
+    type: 'text',
+  })
+  gender: string;
 }
