711428c918390f714059878fe3b38d4d2b150b3f
BryanGallo <bryangallo95@hotmail.com>
2024-10-23 00:42:02 -0500

    Configurando app.setGlobalPrefix con la palabra api
    * Los endpoint se accederan anteponiendo api/

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Configurando para poder anteponer 'api' en el endpoint
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
