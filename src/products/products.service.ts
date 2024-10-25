e5b33e20aab5aad044ff99deb71d6456c91c85ea
BryanGallo <bryangallo95@hotmail.com>
2024-10-24 23:55:50 -0500

    Usando constructor para inyectar nuestro repositorio
    * products.service.ts
    * Inyectamos nuestra entidad Producto como Repository

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
