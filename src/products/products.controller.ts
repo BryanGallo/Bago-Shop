a697990f08049eda05040c00b6f68f8d59b6d257
BryanGallo <bryangallo95@hotmail.com>
2024-11-11 12:44:58 -0500

    Iniciando la actualizacion de producto usando preload
    * falta validacion de slug

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,7 +13,6 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductsService } from './products.service';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { log } from 'console';
 
 @Controller('products')
 export class ProductsController {
@@ -37,10 +36,10 @@ export class ProductsController {
 
   @Patch(':id')
   async update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -69,8 +69,22 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+    if (!product) {
+      throw new NotFoundException(
+        `El producto con el siguiente id: ${id} no existe `,
+      );
+    }
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
   async remove(id: string) {
