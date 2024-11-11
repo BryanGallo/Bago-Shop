b1f31badb646f58f9c7c1c3a3670ff39bf5464de
BryanGallo <bryangallo95@hotmail.com>
2024-11-11 12:57:24 -0500

    BeforeUpdate cuando actualice un Slug
    * asi mantenemos la estructura del slug

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -52,5 +58,14 @@ export class Product {
       .toLowerCase()
       .replaceAll(' ', '_')
       .replaceAll("'", '');
+  }
+  @BeforeUpdate()
+  checkSlugUpdate() {
+    if (this.slug) {
+      this.slug = this.slug
+        .toLowerCase()
+        .replaceAll(' ', '_')
+        .replaceAll("'", '');
+    }
   }
 }
